36e2b834b8e91ae231653ad785973c211c1a8f0a
Jhon Edwin Arias <edwin2arias1@gmail.com>
2024-02-21 18:27:00 -0500

    update to min the js

--- assets/map.js
+++ assets/map.js
@@ -1,46 +1,34 @@
 document.addEventListener("DOMContentLoaded", async function () {
   console.log("DOMContentLoaded");
-  let map = L.map("map").setView([4.351766, -76.106492], 13);
-  let osm = L.tileLayer(
-    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-    {
-      attribution:
-        "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-    }
-  );
-
-  map.addLayer(osm);
-  map.options.minZoom = 13;
-  map.options.maxZoom = 18;
-
-  let ortoSatelital2020 = new L.tileLayer(
-    "ortomosaicosSatelitales/2020/{z}/{x}/{y}.png",
-    {
+  let e = L.map("map").setView([4.351766, -76.106492], 13),
+    t = L.tileLayer(
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      {
+        attribution:
+          "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+      }
+    );
+  e.addLayer(t), (e.options.minZoom = 13), (e.options.maxZoom = 18);
+  let o = new L.tileLayer("ortomosaicosSatelitales/2020/{z}/{x}/{y}.png", {
       minZoom: 13,
       minNativeZoom: 14,
       maxZoom: 18,
-      tms: false,
+      tms: !1,
       attribution: "Pythoma",
-      updateWhenIdle: false,
-      interactive: false,
-    }
-  );
-  let ortoSatelital2023 = new L.tileLayer(
-    "ortomosaicosSatelitales/2023/{z}/{x}/{y}.png",
-    {
+      updateWhenIdle: !1,
+      interactive: !1,
+    }),
+    a = new L.tileLayer("ortomosaicosSatelitales/2023/{z}/{x}/{y}.png", {
       minZoom: 13,
       minNativeZoom: 14,
       maxZoom: 18,
-      tms: false,
+      tms: !1,
       attribution: "Pythoma",
-      updateWhenIdle: false,
-      interactive: false,
-    }
-  );
-  map.addLayer(ortoSatelital2020);
-  let satelitesGroup = {
-    "Imagen Satelital 2020": ortoSatelital2020,
-    "Imagen Satelita 2023": ortoSatelital2023,
-  };
-  let layerControl = L.control.layers(satelitesGroup).addTo(map);
+      updateWhenIdle: !1,
+      interactive: !1,
+    });
+  e.addLayer(o),
+    L.control
+      .layers({ "Imagen Satelital 2020": o, "Imagen Satelita 2023": a })
+      .addTo(e);
 });
